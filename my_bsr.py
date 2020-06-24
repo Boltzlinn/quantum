74c683ff0d747b96d2e2b1c0fa8e060e893ea48a
SHAO Yuelin <shaoyuelin14@mails.ucas.edu.cn>
2020-06-24 14:44:58 +0800

    test my bsr

--- my_bsr.py
+++ my_bsr.py
@@ -31,16 +31,17 @@ def my_bsr_mat_jvp(indices, indptr, shape, priminals, tangents):
 if __name__ == "__main__":
     indptr = jnp.array([0, 2, 3, 6])
     indices = jnp.array([0, 2, 2, 0, 1, 2])
-    shape=(3,3)
+    shape = (6, 6)
+    ones = jnp.ones((2, 2))
     def H(k):
         kx,ky=k
-        data = jnp.array([kx**2, 2.*kx, 3.*ky, 4.*kx*ky, 5.*ky**2, 6.])
+        data = jnp.array([kx**2*ones, 2.*kx*ones, 3.*ky*ones, 4.*kx*ky*ones, 5.*ky**2*ones, 6.*ones])
         return my_bsr_mat(data,indices,indptr,shape)
 
-    H_str=np.array(['kx^2','0.','2.kx','0.','0.','3.ky','4.kx ky', '5.ky^2', '6.']).reshape(shape)
+    #H_str=np.array(['kx^2','0.','2.kx','0.','0.','3.ky','4.kx ky', '5.ky^2', '6.']).reshape(shape)
     k=(1.,1.)
-    print("The sparse 'Hamiltonian' is H(kx,ky)=")
-    print(H_str)
+    #print("The sparse 'Hamiltonian' is H(kx,ky)=")
+    #print(H_str)
     print("The values at k="+str(k)+"is:")
     print(H(k))
     print("The derivatives respective to kx and ky are:")
